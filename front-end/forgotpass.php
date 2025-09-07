391815d7853060e425f5e0a6df16779213328ea4
rudradesa <130157151+rudradesa@users.noreply.github.com>
2025-09-07 18:52:49 +0530

    Update forgotpass.php

--- front-end/forgotpass.php
+++ front-end/forgotpass.php
@@ -292,8 +292,8 @@
               $mail->isSMTP();
               $mail->Host = 'smtp.gmail.com';
               $mail->SMTPAuth = true;
-              $mail->Username = 'eduspace29@gmail.com';
-              $mail->Password = 'fbjw azej qpnk iljl'; // Gmail App Password
+              $mail->Username = '';
+              $mail->Password = ''; // Gmail App Password
               $mail->SMTPSecure = 'tls';
               $mail->Port = 587;
               $mail->setFrom('eduspace29@gmail.com', 'Questly');
